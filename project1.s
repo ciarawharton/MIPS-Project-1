7d7707117c1f9f9ee1a58fd4277d8ff793d0c8aa
Ciara Wharton <whartonciara1299@gmail.com>
2019-10-30 14:04:11 -0400

    Update project1.s
    Program is now able to take in integers and print them.

--- project1.s
+++ project1.s
@@ -15,3 +15,20 @@ li $v0, 4
 la $a0, prompt
 syscall
 
+# get user's integers
+li $v0, 5
+syscall
+
+# store integers into $t0
+move $t0, $v0
+
+# displaying message
+li $v0, 4
+la $a0, message
+syscall
+
+# printing the integers
+li $v0, 1
+move $a0, $t0
+syscall
+
