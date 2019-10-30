4c748eaf648c214894e6c06527efd76381bf9295
Ciara Wharton <whartonciara1299@gmail.com>
2019-10-30 13:56:19 -0400

    Update project1.s
    Attempting to load user input to registers

--- project1.s
+++ project1.s
@@ -1,6 +1,17 @@
 .data
-# getting user input for integers
-prompt: .asciiz "Input integers"
-array:
+
+prompt: .asciiz "Input integers: " # getting user input for integers
+message: .asciiz "\n You entered "
+# x = 02880894
+# N = 26 + (02880894 % 11), N = 31
+# M = N - 10, M = 21
+
 .text
+# gets rid of error message
+.globl main
+main:
+# loading to registers
+li $v0, 4
+la $a0, prompt
 syscall
+
