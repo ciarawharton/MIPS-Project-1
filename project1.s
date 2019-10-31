9ae1e717545fcc45e1603c8a89bbd483c3eead9f
Ciara Wharton <whartonciara1299@gmail.com>
2019-10-31 18:28:24 -0400

    Update project1.s
    Attempting to set size for user input.

--- project1.s
+++ project1.s
@@ -1,10 +1,12 @@
 .data
-
+    counter: .word 0 # counter for the loop
+    input: .space 10 # for the user input
+    inputSize: .word 10 # makes sure user can only write 10 integers
 prompt: .asciiz "Input integers: " # getting user input for integers
-message: .asciiz "\n You entered "
 # x = Howard ID
 # N = 26 + (X % 11), N(base) = 31
 # M = N - 10, M = 21
+# base 31
 
 .text
 # gets rid of error message
@@ -22,31 +24,15 @@ syscall
 # store user integers into $t0
 move $t0, $v0
 
-# displaying message
-li $v0, 4
-la $a0, message
-syscall
+lw $t1, iterator
+
+loop:
+    
+
 
 # printing the integers
-li $v0, 1
+#li $v0, 1
 move $a0, $t0
-syscall
-
-#.code
-# loop instruction to add integers to an array
-loop:
-    ischaracter: # label name/creating a function?
-       xor cx,cx # cx register = counter, setting to 0
-        nop
-        inc cx # incrementing counter
-        cmp cx,10 # comparing cx to the limit
-    # adding the numbers from user input
-        mov eax,
-        add eax, $st0
-        jle ischaracter # loop while less or equal
-    loop ischaracter
-load:
-
 syscall
  
 
