409d962dda4aaa070cddd231065c8dd8c973a9cc
Ciara Wharton <whartonciara1299@gmail.com>
2019-10-31 15:51:26 -0400

    Update project1.s
    No more arrays!

--- project1.s
+++ project1.s
@@ -2,8 +2,8 @@
 
 prompt: .asciiz "Input integers: " # getting user input for integers
 message: .asciiz "\n You entered "
-# x = 02880894
-# N = 26 + (02880894 % 11), N(base) = 31
+# x = Howard ID
+# N = 26 + (X % 11), N(base) = 31
 # M = N - 10, M = 21
 
 .text
@@ -35,21 +35,18 @@ syscall
 #.code
 # loop instruction to add integers to an array
 loop:
-add $t2, $zero, $zero # index of string
+    ischaracter: # label name/creating a function?
+       xor cx,cx # cx register = counter, setting to 0
+        nop
+        inc cx # incrementing counter
+        cmp cx,10 # comparing cx to the limit
+    # adding the numbers from user input
+        mov eax,
+        add eax, $st0
+        jle ischaracter # loop while less or equal
+    loop ischaracter
 load:
-la $s0, array # load address of the array to s0
-la $t0, list # loading address of input
-sll $t3, $t2, 1 # skipping spaces
-sll $t4, $s1, 2
-addu $t0, $t0, $t3 # position of string
-addu $s0, $s0, $t4
-lbu $t1, 0($t0) # loading bytes of input to t1
-addiu $t1, $t1, -48 # converting characters to integer
-sb $t1, 0($s0) # storing bytes into array
-addi $t2, $t2, 1 # increment index of string by 1
-addi $s1, $s1, 1 # increment index of array by 1
-li $v0, 11 # printing input as integers
-#addi $a0, $zero, 0xA
+
 syscall
  
 
