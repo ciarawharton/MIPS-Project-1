073dd76c87ce48f0ee9a6957a7598cf7f6c3a90c
Ciara Wharton <whartonciara1299@gmail.com>
2019-10-31 19:58:33 -0400

    Update project1.s
    Trying something new

--- project1.s
+++ project1.s
@@ -1,9 +1,9 @@
 .data
 
 prompt: .asciiz "Input integers: " # getting user input for integers
-counter: .word 0 # counter for the loop
+#counter: .word 0 # counter for the loop
 input: .space 11 # for the user input
-inputSize: .word 10 # makes sure user can only write 10 integers
+#inputSize: .word 10 # makes sure user can only write 10 integers
 
 # N = 26 + (X % 11), N(base) = 31
 # M = N - 10, M = 21
@@ -24,24 +24,29 @@ lw $t1, counter
 lw $t2, inputSize
 la $t0, input
 
-li $v0, 10
-syscall
-
+#li $v0, 10
+#syscall
 # store user integers into $t0
 move $t4, $a0
 lb $s3, ($t4)
+
+# check values
+beq $t1, 10, print
+
+addi $t1, $t1, 1
+blt $s3, 96 # lowercase (BLT = less than)
+blt $s3, 64 # uppercase (BLT = less than)
+blt $s3, 47 # for 0, BLT = less than
+
+blt $a1, $t2, 58 # less than 10
 # total sum register = 0
 addi $t4, $t4, 1
 lb $s3,($t4)
-syscall
+j check
+#syscall
 
 move $a0, $t0
 syscall
-
-secondLoop: # running through each number in string
-    bgt $a1, 47 # greater than 0
-    blt $a1, $t2, 58 # less than 10
-
 
 
 #97-117
