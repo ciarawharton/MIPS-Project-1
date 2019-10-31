7c4a3478be53b7ea9c612dfbe3ca2e9bddbb3793
Ciara Wharton <whartonciara1299@gmail.com>
2019-10-31 19:40:54 -0400

    Update project1.s
    Deleting the loop for the moment

--- project1.s
+++ project1.s
@@ -8,8 +8,6 @@ inputSize: .word 10 # makes sure user can only write 10 integers
 # N = 26 + (X % 11), N(base) = 31
 # M = N - 10, M = 21
 .text
-# gets rid of error message
-#.globl main
 main:
 # loading to registers
 li $v0, 4
@@ -26,28 +24,15 @@ lw $t1, counter
 lw $t2, inputSize
 la $t0, input
 
-#
 li $v0, 10
 syscall
 
 # store user integers into $t0
-move $t0, $v0
+move $t4, $a0
+lb $s3, ($t4)
 # total sum register = 0
-li $s4, 0
-# s4 is the total sum register
-
-# beginning of loop
-loop:
-    bgt $t2, $t3, end
-    sll $t4, $t2, 2 # counter x 4 store that into t3
-    addu $t4, $t4, $t1 # for the index of the array
-    lb $t7, 0($t4)
-    addu $s8, $s8, $t7 # for the register
-    addi $t2, $t2, 1
-j loop
-
-end:
-    la $a1, input
+addi $t4, $t4, 1
+lb $s3,($t4)
 syscall
 
 move $a0, $t0
