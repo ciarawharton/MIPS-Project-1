2d01e56b563788fb23c66890d72da634a5845934
Ciara Wharton <whartonciara1299@gmail.com>
2019-10-31 23:22:35 -0400

    Update project1.s
    Changed the names of some of the functions, removed extra lines that produced errors.

--- project1.s
+++ project1.s
@@ -13,22 +13,23 @@ li $a1, 11
 syscall
 
 enterInput:
-move $t4, $a0
-lb $s3, ($t4)
+move $t3, $a0
+lb $s2, ($t3)
 
 # check values
 verify:
-beq $t1, 10, calling
+beq $t0, 10, output
 
-addi $t1, $t1, 1
-bge $s3, 97, low # lowercase (BLT = less than)
-bge $s3, 65, up # uppercase (BLT = less than)
-bge $s3, 48, num # for 0, BLT = less than
+sort_values:
+addi $t0, $t0, 1
+bge $s2, 97, lowercaseU # lowercase (BLT = less than)
+bge $s2, 65, uppercaseU # uppercase (BLT = less than)
+bge $s2, 48, numbers # for 0, BLT = less than
 
 # creating a function
 counter:
-addi $t4, $t4, 1
-lb $s3,($t4)
+addi $t3, $t3, 1
+lb $s2,($t3)
 j verify
 
 # uppercase character U
